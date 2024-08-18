6d637d057a73b6c133757bd51ef99858771965ba
Herieieva <herieieva.viktoriia@lll.kpi.ua>
2024-08-18 14:39:41 +0300

    Single linked list from lab #2

---
+++ main.c
@@ -0,0 +1,67 @@
+#include "practicing.h"
+
+#include <stdio.h>
+
+int main() {
+
+  // creating array of random numbers
+  int array_size = 10;
+  int array[array_size];
+
+  GenerateRandom(1, 100, array, array_size);
+
+  // opening file, writing unsorted data, closing
+  FILE *file_ptr;
+
+  file_ptr = fopen("my_file.txt", "w");
+  if (file_ptr == NULL) {
+    printf("Couldn't open file!");
+    return 1;
+  }
+
+  for (int counter = 0; counter < array_size; ++counter) {
+    fprintf(file_ptr, "%d ", array[counter]);
+  }
+  fprintf(file_ptr, "\n");
+
+  fclose(file_ptr);
+
+  // creating single linked list, rewriting numbers from the file
+  SingleListNode *descriptor = CreateList();
+
+  file_ptr = fopen("my_file.txt", "r");
+  if (file_ptr == NULL) {
+    printf("Couldn't open file!");
+    return 1;
+  }
+
+  for (int temp; fscanf(file_ptr, "%d", &temp) != EOF;) {
+    AddBackNode(temp, descriptor);
+  }
+
+  fclose(file_ptr);
+
+  ShowList(descriptor);
+
+  IterativeMergeSort(descriptor); // sorting numbers in the list
+
+  ShowList(descriptor);
+
+  file_ptr = fopen("my_file.txt", "a");
+  if (file_ptr == NULL) {
+    printf("Couldn't open file!");
+    return 1;
+  }
+
+  // writing sorted data from the list to the file
+  for (SingleListNode *ptr = descriptor->next; ptr; ptr = ptr->next) {
+    fprintf(file_ptr, "%d ", ptr->data);
+  }
+  fprintf(file_ptr, "\n");
+
+  fclose(file_ptr);
+
+  DeleteList(descriptor);
+
+  return 0;
+}
---
+++ practicing.h
@@ -0,0 +1,48 @@
+#ifndef PRACTICING_C__PRACTICING_H_
+#define PRACTICING_C__PRACTICING_H_
+
+#include <stdbool.h>
+
+typedef struct SingleListNode
+  {
+  int data;
+  struct SingleListNode* next;
+  } SingleListNode;
+
+SingleListNode* CreateList();
+
+void AddBackNode( int data, SingleListNode* descriptor );
+
+bool IsListEmpty(SingleListNode* descriptor);
+
+bool AddBefore(int position, int data, SingleListNode*);
+
+bool AddAfter( int position, int data, SingleListNode* descriptor );
+
+SingleListNode* ModifyListAt( SingleListNode* descriptor, int position );
+
+bool FindShow (int data, SingleListNode* descriptor);
+
+bool ShowFirst(SingleListNode* descriptor);
+
+void ShowLast(SingleListNode* descriptor);
+
+void ShowList( SingleListNode* descriptor );
+
+bool DeleteAt( int position, SingleListNode* descriptor );
+
+SingleListNode* MergeSortList( SingleListNode* descriptor );
+
+void RewriteFromList( SingleListNode* from,int to[], int start, int end );
+
+void IterativeMergeSort(SingleListNode* descriptor);
+
+void IterativeMerge(int start_left, int left_end, int right_end, SingleListNode* descriptor);
+
+int HowMuchNodes(SingleListNode* descriptor);
+
+void DeleteList( SingleListNode* descriptor );
+
+int* GenerateRandom(int min, int max, int destination[], int size);
+
+#endif //PRACTICING_C__PRACTICING_H_
